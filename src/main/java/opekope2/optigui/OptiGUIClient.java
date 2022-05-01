245d04261c6ca351e8919e7b122c2bd35ad42468
opekope2 <opekope2@gmail.com>
2022-05-01 23:55:18 +0200

    Add mod initializer

---
+++ src/main/java/opekope2/optigui/OptiGUIClient.java
@@ -0,0 +1,27 @@
+package opekope2.optigui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.minecraft.resource.ResourceType;
+import opekope2.optigui.callback.UseCallback;
+import opekope2.optigui.optifinecompat.OptifineResourceLoader;
+
+public class OptiGUIClient implements ClientModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("OptiGUI");
+
+	@Override
+	public void onInitializeClient() {
+		ResourceManagerHelper.get(ResourceType.CLIENT_RESOURCES).registerReloadListener(new OptifineResourceLoader());
+		UseCallback useCallback = new UseCallback();
+		UseBlockCallback.EVENT.register(useCallback);
+		UseEntityCallback.EVENT.register(useCallback);
+	}
+}
