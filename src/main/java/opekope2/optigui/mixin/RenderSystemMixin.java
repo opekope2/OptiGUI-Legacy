d4c335b24135f8622c986cc8cfa6ddab20ca5748
opekope2 <opekope2@gmail.com>
2022-05-01 23:51:50 +0200

    Add mixin

---
+++ src/main/java/opekope2/optigui/mixin/RenderSystemMixin.java
@@ -0,0 +1,18 @@
+package opekope2.optigui.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import net.minecraft.util.Identifier;
+import opekope2.optigui.Replacer;
+
+@Mixin(RenderSystem.class)
+public class RenderSystemMixin {
+	@ModifyVariable(method = "_setShaderTexture(ILnet/minecraft/util/Identifier;)V", at = @At("HEAD"), index = 1)
+	private static Identifier setShaderTextureMixin(Identifier id) {
+		return Replacer.instance.getReplacement(id);
+	}
+}
