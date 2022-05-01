09c841dc3e5a17c94ffeefe1b9e4c0b39eeb6266
opekope2 <opekope2@gmail.com>
2022-05-01 23:52:46 +0200

    Add constants

---
+++ src/main/java/opekope2/optigui/util/TextureResourcePath.java
@@ -0,0 +1,24 @@
+package opekope2.optigui.util;
+
+import net.minecraft.util.Identifier;
+
+public final class TextureResourcePath {
+    public static final Identifier ANVIL = new Identifier("minecraft", "textures/gui/container/anvil.png");
+    public static final Identifier BEACON = new Identifier("minecraft", "textures/gui/container/beacon.png");
+    public static final Identifier BREWING_STAND = new Identifier("minecraft",
+            "textures/gui/container/brewing_stand.png");
+    public static final Identifier CHEST = new Identifier("minecraft", "textures/gui/container/generic_54.png");
+    public static final Identifier CRAFTING_TABLE = new Identifier("minecraft",
+            "textures/gui/container/crafting_table.png");
+    public static final Identifier DISPENSER = new Identifier("minecraft", "textures/gui/container/dispenser.png");
+    public static final Identifier ENCHANTING_TABLE = new Identifier("minecraft",
+            "textures/gui/container/enchanting_table.png");
+    public static final Identifier FURNACE = new Identifier("minecraft", "textures/gui/container/furnace.png");
+    public static final Identifier HOPPER = new Identifier("minecraft", "textures/gui/container/hopper.png");
+    public static final Identifier HORSE = new Identifier("minecraft", "textures/gui/container/horse.png");
+    public static final Identifier SHULKER_BOX = new Identifier("minecraft", "textures/gui/container/shulker_box.png");
+    public static final Identifier VILLAGER = new Identifier("minecraft", "textures/gui/container/villager2.png");
+
+    private TextureResourcePath() {
+    }
+}
